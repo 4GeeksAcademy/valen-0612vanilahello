fa7a8c268e064dbc46592842b66109a71c5609f0
Valen-0612 <vgr230115@gmail.com>
2025-04-29 18:10:38 +0000

    generador dominio valen-06

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombres =['mi','tu','su' ]
+
+  let adjetivos=[ 'brillante','pequeño','veloz']
+
+  let sustativos=[ 'dragon','lobo',' mapache']
+
+let lista = document.getElementById('combinaciones')
+
+  let combinacionesHTML = "";
+
+
+
+  for( let pronombre of pronombres){
+
+  for( let adjetivo of adjetivos){
+  
+  for ( let sustantivo of sustativos ){
+
+   let combinacion = `${pronombre} ${adjetivo} ${sustantivo}.com`;
+
+   combinacionesHTML += `<li class="list-group-item">${combinacion}</li>`;
+   console.log(combinacion)
+
+
+   lista.innerHTML = combinacionesHTML;
+  
+  
+ }
+ }
+ }
 };
+
+
